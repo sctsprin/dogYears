22fe6a7d1727fce55933495b748778f84f9edbc6
James Springfield <sctsprin@me.com>
2014-09-23 13:51:32 +1000

    Initial Commit

---
+++ dogYearsTests/dogYearsTests.swift
@@ -0,0 +1,35 @@
+//
+//  dogYearsTests.swift
+//  dogYearsTests
+//
+//  Created by James Springfield on 23/09/2014.
+//  Copyright (c) 2014 James Springfield. All rights reserved.
+//
+
+import XCTest
+
+class dogYearsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
