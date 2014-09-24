7a74d23698e9fb03cb9ef106adb8c0eaeba14a40
James Springfield <sctsprin@me.com>
2014-09-24 16:50:31 +1000

    First Commit

--- dogYears/ViewController.swift
+++ dogYears/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var dogYearsLabel : UILabel
+    @IBOutlet var enterHumanYearsTextField : UITextField
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender : UIButton) {
+        let convertToDogYears:Double = 7
+        dogYearsLabel.hidden = false
+        let humanYearsToDouble = Double((enterHumanYearsTextField.text as NSString).doubleValue)
+        dogYearsLabel.text = "\(humanYearsToDouble * convertToDogYears)"
+        enterHumanYearsTextField.resignFirstResponder()
+    }
 
+    @IBAction func convertToRealDogYearsButtonPressed(sender : AnyObject) {
+        dogYearsLabel.hidden = false
+        let humanYearsToDouble = Double((enterHumanYearsTextField.text as NSString).doubleValue)
+        if humanYearsToDouble >  2 {
+            dogYearsLabel.text = "\((humanYearsToDouble - 2) * 4 + 21)"
+        }
+        else {
+            dogYearsLabel.text = "\(humanYearsToDouble * 10.5)"
+        }
+        enterHumanYearsTextField.resignFirstResponder()
+        
+    }
+    
 }
 
